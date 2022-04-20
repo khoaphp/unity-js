1d952d3a21149706532f45e2a2867b53c03bf81f
Khoa Pham <khoaphp@yahoo.com>
2022-04-20 23:00:13 +0700

    Add files via upload

---
+++ Bridge.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+public class Bridge : MonoBehaviour
+{
+
+    #if UNITY_WEBGL && !UNITY_EDITOR
+        [DllImport("__Internal")]
+        private static extern void ShowMessage2(string message);
+    #endif
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        #if !UNITY_EDITOR && UNITY_WEBGL
+        WebGLInput.captureAllKeyboardInput = false;
+        SendToJS();
+        #endif
+    }
+
+
+    public void SendToJS() {
+        string MessageToSend = "XXXXX";
+        Debug.Log("Sending message to JavaScript: " + MessageToSend);
+        #if UNITY_WEBGL && !UNITY_EDITOR
+                ShowMessage2(MessageToSend);
+        #endif
+    }
+
+
+}
---
+++ Plugins/yourjavascript.jslib
@@ -0,0 +1,8 @@
+mergeInto(LibraryManager.library, {
+
+	ShowMessage2: function (message) {
+		//window.alert(Pointer_stringify(message));
+		XuLy(Pointer_stringify(message));
+	}
+
+});
